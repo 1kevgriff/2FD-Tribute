92651dbcc02dfb3d8afe2cc4e27557674ea61766
Kevin Griffin <kevin@kevgriffin.com>
2020-07-24 16:20:18 -0400

    test in production

--- 2fdtribute/api/podcast/index.js
+++ 2fdtribute/api/podcast/index.js
@@ -1,16 +1,27 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+let fs = require('fs').promises;
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
+module.exports = async function (context, req) {
+    let fileName = "./podcast.rss";
+    let fileData = await fs.readFile(fileName);
+
+    context.res = {
+        status: 200,
+        headers: {
+            "content-type": "application/rss+xml;"
+        },
+        body: fileData
+    };
+
+    // if (req.query.name || (req.body && req.body.name)) {
+    //     context.res = {
+    //         // status: 200, /* Defaults to 200 */
+    //         body: "Hello " + (req.query.name || req.body.name)
+    //     };
+    // }
+    // else {
+    //     context.res = {
+    //         status: 400,
+    //         body: "Please pass a name on the query string or in the request body"
+    //     };
+    // }
 };
