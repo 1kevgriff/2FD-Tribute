ef2a0b62334bd94912bfce65d003b00aabac3dda
Kevin Griffin <kevin@kevgriffin.com>
2020-07-24 15:48:36 -0400

    blah

---
+++ 2fdtribute/src/.temp/routes.js
@@ -0,0 +1,782 @@
+const c1 = () => import(/* webpackChunkName: "page--src-templates-episode-vue" */ "C:\\repos\\2FD\\2FD-Tribute\\2fdtribute\\src\\templates\\Episode.vue")
+const c2 = () => import(/* webpackChunkName: "page--src-pages-episode-list-vue" */ "C:\\repos\\2FD\\2FD-Tribute\\2fdtribute\\src\\pages\\EpisodeList.vue")
+const c3 = () => import(/* webpackChunkName: "page--node-modules-gridsome-app-pages-404-vue" */ "C:\\repos\\2FD\\2FD-Tribute\\2fdtribute\\node_modules\\gridsome\\app\\pages\\404.vue")
+const c4 = () => import(/* webpackChunkName: "page--src-pages-index-vue" */ "C:\\repos\\2FD\\2FD-Tribute\\2fdtribute\\src\\pages\\Index.vue")
+
+export default [
+  {
+    path: "/your-guide-to-passive-real-estate-investing-using-real-estate-syndications/",
+    component: c1
+  },
+  {
+    path: "/why-women-should-invest-in-real-estate-and-how-to-get-started/",
+    component: c1
+  },
+  {
+    path: "/what-to-do-when-you-get-extra-money/",
+    component: c1
+  },
+  {
+    path: "/when-its-worth-it-to-spend-extra-money/",
+    component: c1
+  },
+  {
+    path: "/using-a-personal-finance-journal-to-reach-your-goals/",
+    component: c1
+  },
+  {
+    path: "/your-guide-to-house-hacking/",
+    component: c1
+  },
+  {
+    path: "/the-best-ways-to-prepare-for-retirement-healthcare-costs/",
+    component: c1
+  },
+  {
+    path: "/tips-for-selling-items-on-offerup-for-extra-income/",
+    component: c1
+  },
+  {
+    path: "/top-five-tips-for-picking-the-right-credit-card/",
+    component: c1
+  },
+  {
+    path: "/when-to-do-your-own-repairs/",
+    component: c1
+  },
+  {
+    path: "/smart-strategies-for-saving-up-for-your-travel-goals/",
+    component: c1
+  },
+  {
+    path: "/teaching-financial-literacy-education-from-an-early-age/",
+    component: c1
+  },
+  {
+    path: "/tips-for-choosing-a-mortgage-insurance-provider/",
+    component: c1
+  },
+  {
+    path: "/tips-for-frugal-travel-in-the-uk/",
+    component: c1
+  },
+  {
+    path: "/tips-for-living-on-an-inconsistent-income/",
+    component: c1
+  },
+  {
+    path: "/tips-for-managing-finances-in-the-military/",
+    component: c1
+  },
+  {
+    path: "/rental-secrets-controlling-what-you-pay-for-rent/",
+    component: c1
+  },
+  {
+    path: "/small-business-survival-tips-for-the-pandemic/",
+    component: c1
+  },
+  {
+    path: "/tips-for-filing-a-tax-extension/",
+    component: c1
+  },
+  {
+    path: "/tips-for-managing-your-business-finances/",
+    component: c1
+  },
+  {
+    path: "/tips-for-raising-money-savvy-kids/",
+    component: c1
+  },
+  {
+    path: "/tips-for-saving-money-on-healthcare/",
+    component: c1
+  },
+  {
+    path: "/tips-for-spending-money-in-groups/",
+    component: c1
+  },
+  {
+    path: "/upgrading-your-financial-operating-systems/",
+    component: c1
+  },
+  {
+    path: "/parenting-and-technology-raising-kids-in-the-digital-era/",
+    component: c1
+  },
+  {
+    path: "/re-evaluating-expenses-after-covid-19/",
+    component: c1
+  },
+  {
+    path: "/supporting-anti-racism-through-giving/",
+    component: c1
+  },
+  {
+    path: "/our-stimulus-check-review-small-business-and-personal/",
+    component: c1
+  },
+  {
+    path: "/putting-the-fun-back-in-frugality/",
+    component: c1
+  },
+  {
+    path: "/making-better-financial-decisions-to-reach-success-faster/",
+    component: c1
+  },
+  {
+    path: "/mysteries-and-lies-about-federal-student-loans/",
+    component: c1
+  },
+  {
+    path: "/our-tiller-money-app-review/",
+    component: c1
+  },
+  {
+    path: "/how-to-work-on-your-business-instead-of-in-your-business/",
+    component: c1
+  },
+  {
+    path: "/lifestyle-deflation-for-a-better-life/",
+    component: c1
+  },
+  {
+    path: "/managing-financial-expectations-during-divorce/",
+    component: c1
+  },
+  {
+    path: "/figuring-out-whats-next-in-financial-independence-and-financial-lifestyle-planning/",
+    component: c1
+  },
+  {
+    path: "/how-blue-collar-workers-can-achieve-the-american-dream/",
+    component: c1
+  },
+  {
+    path: "/how-to-be-a-good-human-during-the-pandemic/",
+    component: c1
+  },
+  {
+    path: "/how-to-de-risk-the-move-to-self-employment/",
+    component: c1
+  },
+  {
+    path: "/getting-real-estate-deals-for-0-out-of-pocket/",
+    component: c1
+  },
+  {
+    path: "/how-to-form-a-healthy-relationship-with-money/",
+    component: c1
+  },
+  {
+    path: "/how-to-start-a-business-on-a-budget/",
+    component: c1
+  },
+  {
+    path: "/how-to-talk-about-money-with-your-spouse/",
+    component: c1
+  },
+  {
+    path: "/2fd-061-financial-abuse-improving-your-credit-score-single-parents-special-needs-assistance-freelancing-frugal-self-defense-and-short-term-investments/",
+    component: c1
+  },
+  {
+    path: "/how-to-do-a-profit-first-distribution/",
+    component: c1
+  },
+  {
+    path: "/how-to-get-started-with-social-investing/",
+    component: c1
+  },
+  {
+    path: "/how-to-implement-a-money-management-system/",
+    component: c1
+  },
+  {
+    path: "/how-to-save-money-at-amusement-parks/",
+    component: c1
+  },
+  {
+    path: "/generating-passive-income-with-real-estate-investing/",
+    component: c1
+  },
+  {
+    path: "/getting-a-mortgage-without-a-credit-score/",
+    component: c1
+  },
+  {
+    path: "/helpful-tips-for-buying-new-appliances/",
+    component: c1
+  },
+  {
+    path: "/helpful-tips-for-smart-online-shopping/",
+    component: c1
+  },
+  {
+    path: "/investing-in-mortgage-backed-securities/",
+    component: c1
+  },
+  {
+    path: "/is-disney-worth-the-investment/",
+    component: c1
+  },
+  {
+    path: "/2fd-085-active-military-veteran-and-government-finances-financial-independence-in-hawaii-and-finding-the-best-dates-to-fly/",
+    component: c1
+  },
+  {
+    path: "/frugal-tips-for-building-a-garage-gym/",
+    component: c1
+  },
+  {
+    path: "/frugal-tips-for-buying-lawn-maintenance-equipment/",
+    component: c1
+  },
+  {
+    path: "/2fd-059-the-path-towards-wealth-401k-changes-saving-on-groceries-tackling-debt-new-years-resolutions-and-cattle/",
+    component: c1
+  },
+  {
+    path: "/an-update-on-the-payroll-protection-program-for-small-business-employees/",
+    component: c1
+  },
+  {
+    path: "/expert-advice-for-escaping-a-broken-mindset/",
+    component: c1
+  },
+  {
+    path: "/2fd-060-managing-your-own-finances-being-poor-vs-broke-investment-expenses-immigration-challenges-and-roth-401ks/",
+    component: c1
+  },
+  {
+    path: "/2fd-072-crush-debt-how-to-invest-debt-payoff-strategies-investing-vs-debt-payoff-utma-ugma-accounts/",
+    component: c1
+  },
+  {
+    path: "/2fd-073-mini-retirements-1-meals-sabbaticals-from-the-workforce-protecting-your-wealth-work-as-an-option/",
+    component: c1
+  },
+  {
+    path: "/2fd-087-debt-and-mortgage-free-in-four-years-negative-net-worth-to-750k-and-estate-planning/",
+    component: c1
+  },
+  {
+    path: "/creating-multiple-savings-accounts-to-reach-your-goals/",
+    component: c1
+  },
+  {
+    path: "/evaluating-opportunity-cost-with-business-travel/",
+    component: c1
+  },
+  {
+    path: "/2fd-079-the-adventures-around-you-debt-opportunity-cost-diy-vs-paid-services-and-house-sitting/",
+    component: c1
+  },
+  {
+    path: "/2fd-083-timeshare-exit-risks-of-timeshares-outsourcing-help-and-putting-quality-back-into-your-diet/",
+    component: c1
+  },
+  {
+    path: "/2fd-086-financially-independent-retired-early-fire-community-non-fire-neighbors-switching-to-internet-cable-tv/",
+    component: c1
+  },
+  {
+    path: "/choose-fi-your-blueprint-to-financial-independence/",
+    component: c1
+  },
+  {
+    path: "/do-it-yourself-debt-relief-options/",
+    component: c1
+  },
+  {
+    path: "/eliminating-your-mortgage-insurance-premium/",
+    component: c1
+  },
+  {
+    path: "/2fd-067-financial-fraud-who-to-trust-with-your-money-stock-market-correction-fiduciary-advisors/",
+    component: c1
+  },
+  {
+    path: "/2fd-091-find-freedom-by-learning-to-be-better-stewards-of-time-talent-and-treasures/",
+    component: c1
+  },
+  {
+    path: "/a-guide-to-living-a-life-of-frugality/",
+    component: c1
+  },
+  {
+    path: "/becoming-a-giver-while-pursuing-financial-independence/",
+    component: c1
+  },
+  {
+    path: "/building-a-kids-survival-kit-for-isolation/",
+    component: c1
+  },
+  {
+    path: "/2fd-062-maximizing-income-intentional-spending-getting-a-raise-frugal-divorce-and-personal-profit/",
+    component: c1
+  },
+  {
+    path: "/2fd-063-paying-off-student-loans-401k-early-withdrawal-concerns-frugal-internet-tv-streaming/",
+    component: c1
+  },
+  {
+    path: "/2fd-071-wall-street-life-financial-literacy-achieving-goals-faster-and-family-finance-meetings/",
+    component: c1
+  },
+  {
+    path: "/2fd-077-finding-a-fi-club-airbnbing-your-house-and-knowing-your-operating-leverage/",
+    component: c1
+  },
+  {
+    path: "/2fd-084-investment-allocation-index-funds-vs-stock-cryptocurrency-and-long-term-care-insurance/",
+    component: c1
+  },
+  {
+    path: "/all-about-buying-and-repairing-used-items/",
+    component: c1
+  },
+  {
+    path: "/auditing-your-utility-bills-to-save-money/",
+    component: c1
+  },
+  {
+    path: "/balancing-multiple-and-competing-money-goals/",
+    component: c1
+  },
+  {
+    path: "/choosing-a-mortgage-interest-rate/",
+    component: c1
+  },
+  {
+    path: "/choosing-an-airline-loyalty-program/",
+    component: c1
+  },
+  {
+    path: "/2fd-065-early-retirement-life-mindfulness-how-to-be-happy-tax-filing-tips/",
+    component: c1
+  },
+  {
+    path: "/2fd-066-full-time-traveling-cataloging-your-keepsakes-rv-living-costs-and-boondocking/",
+    component: c1
+  },
+  {
+    path: "/2fd-068-frugal-experiences-lifestyle-overhaul-free-events-and-taxes-for-giving-money/",
+    component: c1
+  },
+  {
+    path: "/2fd-069-geographic-arbitrage-retiring-aboard-managing-rental-property-remotely-and-savings-statistics/",
+    component: c1
+  },
+  {
+    path: "/2fd-074-compound-debt-payoff-side-hustles-practical-budgeting-and-frugal-lawn-care/",
+    component: c1
+  },
+  {
+    path: "/2fd-081-buy-and-hold-investing-reinventing-yourself-and-changes-to-529-plans/",
+    component: c1
+  },
+  {
+    path: "/all-about-tax-qualified-investment-options/",
+    component: c1
+  },
+  {
+    path: "/episode-list/",
+    component: c2
+  },
+  {
+    path: "/2fd-038-the-money-youre-leaving-on-the-table-with-john-wedding/",
+    component: c1
+  },
+  {
+    path: "/2fd-044-finance-nerds-unite-sharing-your-financial-journey-with-phillip-taylor/",
+    component: c1
+  },
+  {
+    path: "/2fd-046-safely-withdrawing-your-investments-in-retirement-with-early-retirement-dude/",
+    component: c1
+  },
+  {
+    path: "/2fd-070-money-for-meaning-death-regrets-breaking-fear-and-cheap-movies/",
+    component: c1
+  },
+  {
+    path: "/2fd-075-house-hacking-real-estate-investing-business-finances-and-bird-dogging/",
+    component: c1
+  },
+  {
+    path: "/2fd-078-the-startup-life-money-for-what-matters-and-hotel-alternatives/",
+    component: c1
+  },
+  {
+    path: "/2fd-082-downsizing-evaluating-investment-property-real-estate-misconceptions-and-mastermind-groups/",
+    component: c1
+  },
+  {
+    path: "/2fd-101-giving-hope-with-your-feel-good-story-of-the-day/",
+    component: c1
+  },
+  {
+    path: "/all-about-frugality-and-freelancing/",
+    component: c1
+  },
+  {
+    path: "/2fd-031-setting-up-your-frugal-kitchen-with-the-frugal-girl/",
+    component: c1
+  },
+  {
+    path: "/2fd-088-debt-collectors-credit-report-errors-identity-and-property-theft/",
+    component: c1
+  },
+  {
+    path: "/2fd-010-all-about-kids-with-our-guest-erik-olson/",
+    component: c1
+  },
+  {
+    path: "/2fd-036-blogging-as-a-side-hustle-with-j-money/",
+    component: c1
+  },
+  {
+    path: "/2fd-040-the-reasons-for-homeschooling-with-the-frugal-girl/",
+    component: c1
+  },
+  {
+    path: "/2fd-041-tapping-into-your-freelancing-potential-with-brennan-dunn/",
+    component: c1
+  },
+  {
+    path: "/2fd-053-finding-a-health-insurance-plan-that-fits-you/",
+    component: c1
+  },
+  {
+    path: "/2fd-054-saving-money-buying-selling-and-refinancing-a-home/",
+    component: c1
+  },
+  {
+    path: "/2fd-056-saving-money-the-best-way-for-your-goals/",
+    component: c1
+  },
+  {
+    path: "/2fd-064-pension-plans-calculating-retirement-frugal-valentines-day-ideas/",
+    component: c1
+  },
+  {
+    path: "/2fd-076-retirement-accounts-self-directed-iras-and-world-travel/",
+    component: c1
+  },
+  {
+    path: "/2fd-080-money-in-relationships-marriage-counseling-and-frugal-jewelry/",
+    component: c1
+  },
+  {
+    path: "/2fd-104-everything-you-need-to-know-about-fi-summit/",
+    component: c1
+  },
+  {
+    path: "/2fd-106-how-to-market-your-business-on-a-budget/",
+    component: c1
+  },
+  {
+    path: "/2fd-033-taking-the-frugal-leap-with-mrs-frugalwoods/",
+    component: c1
+  },
+  {
+    path: "/2fd-037-managing-your-personal-finances-with-mint-com/",
+    component: c1
+  },
+  {
+    path: "/2fd-042-making-major-life-changes-with-todd-nevins/",
+    component: c1
+  },
+  {
+    path: "/2fd-043-things-to-consider-about-a-college-education/",
+    component: c1
+  },
+  {
+    path: "/2fd-052-quality-of-life-before-standard-of-life/",
+    component: c1
+  },
+  {
+    path: "/2fd-055-the-spending-and-saving-habits-of-americans/",
+    component: c1
+  },
+  {
+    path: "/2fd-095-review-of-financially-stupid-people-are-everywhere/",
+    component: c1
+  },
+  {
+    path: "/2fd-105-ten-personal-finance-tips-for-technology-professionals/",
+    component: c1
+  },
+  {
+    path: "/2fd-108-how-to-survive-with-only-one-car/",
+    component: c1
+  },
+  {
+    path: "/2fd-113-the-four-keys-to-abundant-frugal-living/",
+    component: c1
+  },
+  {
+    path: "/2fd-115-10-keys-to-building-a-financial-plan/",
+    component: c1
+  },
+  {
+    path: "/2fd-004-cut-back-on-your-transportation-costs/",
+    component: c1
+  },
+  {
+    path: "/2fd-007-take-advantage-of-your-retirement-accounts/",
+    component: c1
+  },
+  {
+    path: "/2fd-034-listener-case-study-with-francis-pasket/",
+    component: c1
+  },
+  {
+    path: "/2fd-039-listener-case-study-with-aimee-knight/",
+    component: c1
+  },
+  {
+    path: "/2fd-045-listener-case-study-with-sal-hernandez/",
+    component: c1
+  },
+  {
+    path: "/2fd-051-listener-case-study-with-kyle-anderson/",
+    component: c1
+  },
+  {
+    path: "/2fd-057-getting-started-with-real-estate-investing/",
+    component: c1
+  },
+  {
+    path: "/2fd-096-coder-cruise-and-frugal-cruise-hacks/",
+    component: c1
+  },
+  {
+    path: "/2fd-097-pursuing-fire-while-traveling-full-time/",
+    component: c1
+  },
+  {
+    path: "/2fd-102-managing-your-personal-and-business-finances/",
+    component: c1
+  },
+  {
+    path: "/2fd-109-all-about-incorporating-2-frugal-dudes/",
+    component: c1
+  },
+  {
+    path: "/2019-in-review-and-the-2020-vision/",
+    component: c1
+  },
+  {
+    path: "/2fd-001-lets-start-saving-some-money/",
+    component: c1
+  },
+  {
+    path: "/2fd-011-whats-your-money-backup-plan/",
+    component: c1
+  },
+  {
+    path: "/2fd-015-you-need-an-accountability-partner/",
+    component: c1
+  },
+  {
+    path: "/2fd-016-military-finances-with-russ-gates/",
+    component: c1
+  },
+  {
+    path: "/2fd-020-unshakeable-book-study-part-1/",
+    component: c1
+  },
+  {
+    path: "/2fd-022-unshakeable-book-study-part-2/",
+    component: c1
+  },
+  {
+    path: "/2fd-023-unshakeable-book-study-part-3/",
+    component: c1
+  },
+  {
+    path: "/2fd-024-unshakeable-book-study-part-4/",
+    component: c1
+  },
+  {
+    path: "/2fd-025-unshakeable-book-study-part-5/",
+    component: c1
+  },
+  {
+    path: "/2fd-026-unshakeable-book-study-part-6/",
+    component: c1
+  },
+  {
+    path: "/2fd-028-discussing-early-retirement-with-others/",
+    component: c1
+  },
+  {
+    path: "/2fd-029-the-financial-independence-online-community/",
+    component: c1
+  },
+  {
+    path: "/2fd-048-budgeting-for-a-vacation-abroad/",
+    component: c1
+  },
+  {
+    path: "/2fd-049-alternative-investments-for-your-portfolio/",
+    component: c1
+  },
+  {
+    path: "/2fd-050-looking-back-after-one-year/",
+    component: c1
+  },
+  {
+    path: "/2fd-058-experiencing-freedom-before-financial-freedom/",
+    component: c1
+  },
+  {
+    path: "/2fd-099-live-from-fincon-pt-2/",
+    component: c1
+  },
+  {
+    path: "/2fd-103-conducting-a-personal-budget-analysis/",
+    component: c1
+  },
+  {
+    path: "/2fd-107-frugal-tips-for-tax-season/",
+    component: c1
+  },
+  {
+    path: "/2fd-111-how-we-budget-our-money/",
+    component: c1
+  },
+  {
+    path: "/2fd-116-when-public-transit-makes-sense/",
+    component: c1
+  },
+  {
+    path: "/2fd-005-thinking-before-you-buy/",
+    component: c1
+  },
+  {
+    path: "/2fd-008-get-rid-of-stuff/",
+    component: c1
+  },
+  {
+    path: "/2fd-012-vacations-on-a-budget/",
+    component: c1
+  },
+  {
+    path: "/2fd-021-credit-card-rewards-hacking/",
+    component: c1
+  },
+  {
+    path: "/2fd-032-the-psychology-of-investing/",
+    component: c1
+  },
+  {
+    path: "/2fd-047-understanding-your-credit-report/",
+    component: c1
+  },
+  {
+    path: "/2fd-090-fuel-your-entrepreneur-aspirations/",
+    component: c1
+  },
+  {
+    path: "/2fd-110-life-as-a-renter/",
+    component: c1
+  },
+  {
+    path: "/2fd-112-planning-for-senior-housing/",
+    component: c1
+  },
+  {
+    path: "/2fd-114-the-next-financial-summit/",
+    component: c1
+  },
+  {
+    path: "/2fd089-simplify-college-529-plan-savings/",
+    component: c1
+  },
+  {
+    path: "/2fd-002-budget-budget-budget/",
+    component: c1
+  },
+  {
+    path: "/2fd-003-owning-vs-renting/",
+    component: c1
+  },
+  {
+    path: "/2fd-006-health-insurance-options/",
+    component: c1
+  },
+  {
+    path: "/2fd-009-your-grocery-budget/",
+    component: c1
+  },
+  {
+    path: "/2fd-013-save-some-energy/",
+    component: c1
+  },
+  {
+    path: "/2fd-014-starting-a-business/",
+    component: c1
+  },
+  {
+    path: "/2fd-018-post-cruise-update/",
+    component: c1
+  },
+  {
+    path: "/2fd-027-surviving-early-retirement/",
+    component: c1
+  },
+  {
+    path: "/2fd-030-investing-in-yourself/",
+    component: c1
+  },
+  {
+    path: "/2fd-035-charity-and-giving/",
+    component: c1
+  },
+  {
+    path: "/2fd-092-breaking-down-bankruptcy/",
+    component: c1
+  },
+  {
+    path: "/2fd-093-peeling-back-frugality/",
+    component: c1
+  },
+  {
+    path: "/2fd-094-affordable-world-travel/",
+    component: c1
+  },
+  {
+    path: "/2fd-098-live-from-fincon/",
+    component: c1
+  },
+  {
+    path: "/2fd-100-the-100th-episode/",
+    component: c1
+  },
+  {
+    path: "/2fd-017-random-money/",
+    component: c1
+  },
+  {
+    path: "/2fd-019-choosing-insurance/",
+    component: c1
+  },
+  {
+    name: "404",
+    path: "/404/",
+    component: c3
+  },
+  {
+    name: "home",
+    path: "/",
+    component: c4
+  },
+  {
+    name: "*",
+    path: "*",
+    component: c3
+  }
+]
